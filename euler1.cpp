d126aa1d3da25c48a34c10d0f9eb3b77ffe05812
Travace Reese <tsuk13@gmail.com>
2012-09-07 01:01:21 -0400

    euler1 working for n=10

--- euler1.cpp
+++ euler1.cpp
@@ -3,6 +3,12 @@
 using std::cout;
 
 int main(){
-	cout << "Success!\n";
+	int n = 10; //how high we are going
+	int sum = 0; //keep running total
+	for(int i =1; i<n; ++i){
+		if(i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	cout << "Total up to " << n << " is " << sum <<"\n";
 	return 0;
 }
